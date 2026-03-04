73ae1d1bda3eed4fb7cab0d5e514527c12cff27d
robertonavarropm <robertonavarropm@gmail.com>
2026-03-04 19:48:39 -0300

    codigo movimiento actualizado

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z);
+
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
