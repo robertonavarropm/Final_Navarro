ec0e6543396885cad2c385f4eb1d2d888817b6b5
robertonavarropm <robertonavarropm@gmail.com>
2026-03-04 20:08:31 -0300

    scriptable object enemy data v1

--- Assets/Scriptablesobjects/EnemyData.cs
+++ Assets/Scriptablesobjects/EnemyData.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
-public class NewScriptableObjectScript : ScriptableObject
+[CreateAssetMenu(menuName = "Bomberman/Enemy Data", fileName = "EnemyData_")]
+public class EnemyData : ScriptableObject
 {
-    
+    [Header("Stats")]
+    public int maxHp = 1;
+    public float moveSpeed = 3f;
+    public int contactDamage = 1;
+
+    [Header("Movement (Bomberman-like)")]
+    public float obstacleCheckDistance = 0.6f;
+    public float directionChangeCooldown = 0.2f;
 }
