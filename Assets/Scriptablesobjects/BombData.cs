a5784fe3e9ce9817f28e0d0eb9827962632cdf32
robertonavarropm <robertonavarropm@gmail.com>
2026-03-04 20:52:53 -0300

    bomb script creado

--- Assets/Scriptablesobjects/BombData.cs
+++ Assets/Scriptablesobjects/BombData.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BombData", menuName = "Scriptable Objects/BombData")]
+[CreateAssetMenu(menuName = "Bomberman/Bomb Data", fileName = "BombData_")]
 public class BombData : ScriptableObject
 {
-    
+    [Header("Timing")]
+    public float fuseTime = 2.0f;      // tiempo hasta explotar
+    public float explosionTime = 0.2f; // duración visual/daño (si usás hitboxes)
+
+    [Header("Explosion")]
+    public int range = 2;              // alcance en celdas
+    public float cellSize = 1f;        // tamaño de celda/grilla
 }
